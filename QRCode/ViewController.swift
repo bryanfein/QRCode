016e37df9321712b7197053d51db46e750361ecd
bryanfein <bryanfein@gmail.com>
2017-05-19 01:54:08 -0400

    Created a QRCode Generator

--- QRCode/ViewController.swift
+++ QRCode/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //Variables
+    var qrCodeImage: CIImage!
+    
+    
+    //Outlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imgQRCode: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var btnAction: UIButton!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
+    
+    
+    func displayQRCodeImage(){
+        
+        let scaleX = imgQRCode.frame.size.width / qrCodeImage.extent.size.width
+        let scaleY = imgQRCode.frame.size.height / qrCodeImage.extent.size.width
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let transformedImage = CGAffineTransform(scaleX: scaleX, y: scaleY)
+        let scaled = qrCodeImage.applying(transformedImage)
+        
+        imgQRCode.image = UIImage(ciImage: scaled)
+        
+
+        
     }
-
-
+    
+    @IBAction func generateButton(_ sender: UIButton) {
+        if qrCodeImage == nil {
+            
+            
+            //nothing will happen is textfield is empty
+            if textField.text == "" {
+                return
+            }
+            
+            let data = textField.text?.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+            
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            
+            filter?.setValue(data, forKey: "inputMessage")
+            filter?.setValue("Q", forKey: "inputCorrectionLevel")
+            
+            qrCodeImage = filter?.outputImage
+            
+            //convert the output CIImage to an UIImage object
+//            imgQRCode.image = UIImage(ciImage: qrCodeImage)
+            displayQRCodeImage()
+            
+            textField.resignFirstResponder()
+            
+            btnAction.setTitle("Clear", for: .normal)
+            slider.isHidden = false
+            
+        }
+        else {
+            imgQRCode.image = nil
+            qrCodeImage = nil
+            btnAction.setTitle("Generate", for: .normal)
+            
+        }
+//        
+//        textField.isEnabled = !textField.isEnabled
+//        slider.isHidden = !slider.isHidden
+        
+    }
+    
+    @IBAction func changeImageViewScale(_ sender: UISlider) {
+        
+//        imgQRCode.transform = CGAffineTransform(scaleX: CGFloat(slider.value), y: CGFloat(slider.value))
+//        slider.isHidden = false
+//        
+    }
 }
 
